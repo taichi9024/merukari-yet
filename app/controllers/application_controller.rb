5b47d7ebb10f3021a28e0369c21e7686e23406ac
taichi9024 <ebh48978@gmail.com>
2021-01-01 19:10:12 +0900

    finish landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    layout :setting
+
+    private 
+    def setting
+        if params[:controller].include?("admin")
+            return "admin"
+        else
+            return "customer"
+        end
+    end
 end
